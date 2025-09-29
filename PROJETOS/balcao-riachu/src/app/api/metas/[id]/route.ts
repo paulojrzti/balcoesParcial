59e058a6a69878ecacd416f9e345da0e4939599d
paulo junior waquimaker <paulojrzti@gmail.com>
2025-09-29 15:00:13 -0300

    corrigindo metas/id/route.ts

--- PROJETOS/balcao-riachu/src/app/api/metas/[id]/route.ts
+++ PROJETOS/balcao-riachu/src/app/api/metas/[id]/route.ts
@@ -1,36 +1,47 @@
-import { NextResponse } from "next/server";
-import {prisma} from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
 
-export async function GET(_: Request, { params }: { params: { id: string } }) {
+type Params = {
+  params: Promise<{ id: string }>;
+};
+
+// GET - obter meta por ID
+export async function GET(_: NextRequest, { params }: Params) {
+  const { id } = await params;
+
   const meta = await prisma.metaMes.findUnique({
-    where: { id: Number(params.id) },
+    where: { id: Number(id) },
     include: { metasDia: true, categoria: true },
   });
 
-  if (!meta)
+  if (!meta) {
     return NextResponse.json({ error: "Meta não encontrada" }, { status: 404 });
+  }
+
   return NextResponse.json(meta);
 }
 
-export async function PUT(
-  req: Request,
-  { params }: { params: { id: string } }
-) {
+// PUT - atualizar meta
+export async function PUT(req: NextRequest, { params }: Params) {
+  const { id } = await params;
   const body = await req.json();
   const { valor } = body;
 
   const meta = await prisma.metaMes.update({
-    where: { id: Number(params.id) },
+    where: { id: Number(id) },
     data: { valor },
   });
 
   return NextResponse.json(meta);
 }
 
-export async function DELETE(
-  _: Request,
-  { params }: { params: { id: string } }
-) {
-  await prisma.metaMes.delete({ where: { id: Number(params.id) } });
+// DELETE - remover meta
+export async function DELETE(_: NextRequest, { params }: Params) {
+  const { id } = await params;
+
+  await prisma.metaMes.delete({
+    where: { id: Number(id) },
+  });
+
   return NextResponse.json({ message: "Meta removida com sucesso" });
 }
