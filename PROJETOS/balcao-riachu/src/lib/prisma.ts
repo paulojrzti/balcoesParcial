bc2be4db7c447da2a968906e5e202cecea419be0
paulo junior waquimaker <paulojrzti@gmail.com>
2025-09-29 16:09:40 -0300

    migrando banco

--- PROJETOS/balcao-riachu/src/lib/prisma.ts
+++ PROJETOS/balcao-riachu/src/lib/prisma.ts
@@ -1,13 +1,10 @@
+// lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-export const prisma =
-  globalThis.prisma ??
-  new PrismaClient({
-    log: ["query", "error", "warn"],
-  });
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
